8b08326a08d11654368f2faced2a11153eefdea5
Fledrox <fledrox@gmail.com>
2025-07-06 16:42:15 +0200

    add particles

--- suivi_du_niveau_dalcool.gd
+++ suivi_du_niveau_dalcool.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _process(delta: float) -> void:
 	$ProgressBar.value = get_parent().find_child("ZonePartition").consecutive_hits
+	if $ProgressBar.value == $ProgressBar.max_value:
+		$hitspace.show()
+	else:
+		$hitspace.hide()
